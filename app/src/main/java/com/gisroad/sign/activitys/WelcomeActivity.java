ead9f8d6ed684fb70fb22a94aa233afbf504e0db
stevefat <ngh8897@gmail.com>
2017-03-11 15:39:08 +0800

    Update gradle for 2.3 and modify permissions bug

--- app/src/main/java/com/gisroad/sign/activitys/WelcomeActivity.java
+++ app/src/main/java/com/gisroad/sign/activitys/WelcomeActivity.java
@@ -20,21 +20,7 @@ public class WelcomeActivity extends BaseActivity {
     @Override
     protected void initView() {
         askMultiplePermission();
-        //提示欢迎使用这个查询系统
-        //如果数据库有数据，就直接显示 ---------MainActivity
-        //没有数据走更新数据   ----LoginActivity
 
-        int countsize = DepartmentManager.getInstance().select().size();
-
-        if (countsize > 0) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
-        //关闭界面
-        finish();
     }
 
     @Override
@@ -50,5 +36,25 @@ public class WelcomeActivity extends BaseActivity {
     public void onPause() {
         super.onPause();
         MobclickAgent.onPause(this);
+    }
+
+    @Override
+    public void getData() {
+        super.getData();
+        //提示欢迎使用这个查询系统
+        //如果数据库有数据，就直接显示 ---------MainActivity
+        //没有数据走更新数据   ----LoginActivity
+
+        int countsize = DepartmentManager.getInstance().select().size();
+
+        if (countsize > 0) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        //关闭界面
+        finish();
     }
 }
